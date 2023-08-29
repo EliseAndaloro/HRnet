a9ead0da1dca58cbcf2d2a0aefccfdae2c76bf0a
Elise Andaloro <e-andaloro@sfi.fr>
2023-08-29 12:24:14 +0200

    creation structure react + page CurrentEmployee & CreateEmployees

---
+++ hrnet-react/src/pages/createEmployees/CreateEmployees.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./createEmployees.css";
+
+/**
+ * Composant pour la création d'un employé
+ * @function
+ * @returns {JSX.Element} - Le composant de création d'un employé
+ */
+const CreateEmployees = () => {
+    return (
+        <main>
+            <h2 className="title">Create Employee</h2>
+        </main>
+    );
+};
+
+export default CreateEmployees;
---
+++ hrnet-react/src/pages/currentEmployee/CurrentEmployee.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./currentEmployee.css";
+
+/**
+ * Composant pour afficher la liste des employés actuels
+ * @function
+ * @returns {JSX.Element} - Le composant pour afficher la liste des employés actuels
+ */
+const CurrentEmployees = () => {
+    return (
+        <>
+            <section className="enteteTable">
+                <h2 className="title">Current Employees</h2>
+            </section>
+        </>
+    );
+};
+
+export default CurrentEmployees;
